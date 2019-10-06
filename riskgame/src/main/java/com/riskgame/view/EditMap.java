33c69dc9e1ae8b228388bf556acf825660ce447d
Zankhana Patel <zankhanapatel1995.zp@gmail.com>
2019-10-06 00:42:56 -0400

    Update EditMap.java

--- riskgame/src/main/java/com/riskgame/view/EditMap.java
+++ riskgame/src/main/java/com/riskgame/view/EditMap.java
@@ -1,6 +1,13 @@
 package com.riskgame.view;
 
 import javax.swing.JFrame;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JRadioButton;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
 public class EditMap extends JFrame {
 	
@@ -10,6 +17,25 @@ public class EditMap extends JFrame {
 	 */
 	public EditMap() {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		JRadioButton rdbtnEditNewFile = new JRadioButton("Edit new File");
+		
+		JRadioButton rdbtnBrowseExistingFile = new JRadioButton("Browse Existing file");
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(60)
+					.addComponent(rdbtnEditNewFile)
+					.addGap(35)
+					.addComponent(rdbtnBrowseExistingFile))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addComponent(rdbtnEditNewFile)
+				.addComponent(rdbtnBrowseExistingFile)
+		);
+		getContentPane().setLayout(groupLayout);
 		
 		
 	}
