8d58e8533277640e3d7d432b516681c7f9a30b43
Zankhana Patel <zankhanapatel1995.zp@gmail.com>
2019-10-07 21:03:34 -0400

    Territory pojo updated
    TerritoryIndex added

--- riskgame/src/main/java/com/riskgame/model/Territory.java
+++ riskgame/src/main/java/com/riskgame/model/Territory.java
@@ -2,7 +2,15 @@ package com.riskgame.model;
 
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Territory {
+	private int territoryIndex;
 	private String territoryName;
 	private List<String> neighbourTerritories;
 
