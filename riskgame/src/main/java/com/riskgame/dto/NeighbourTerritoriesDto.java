575bbe1770ec4a78baa7601850435a6fc9512293
Kosha Patel <koshapatel1045@gmail.com>
2019-10-12 13:50:03 -0400

    NeighbourTerritoriesDto added

---
+++ riskgame/src/main/java/com/riskgame/dto/NeighbourTerritoriesDto.java
@@ -0,0 +1,16 @@
+package com.riskgame.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class NeighbourTerritoriesDto {
+
+	private int id;
+	private String countryName;
+	private String countryNeighbourName;
+
+}
