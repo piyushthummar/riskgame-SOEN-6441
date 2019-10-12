daf8abdfa298ae15b49ddc8bad106e6dd6a0bfa7
Kosha Patel <koshapatel1045@gmail.com>
2019-10-12 13:47:34 -0400

    Added DTO for view side named countryDTO

---
+++ riskgame/src/main/java/com/riskgame/dto/ContinentDto.java
@@ -0,0 +1,16 @@
+package com.riskgame.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ContinentDto {
+	
+	private int id;
+	private String continentName;
+	private int continentValue;
+
+}
