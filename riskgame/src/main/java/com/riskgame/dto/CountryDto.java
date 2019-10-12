439e89495a1ddb9b812cf4219c1d08e2177b1a75
Kosha Patel <koshapatel1045@gmail.com>
2019-10-12 13:48:37 -0400

    CountryDTO added

---
+++ riskgame/src/main/java/com/riskgame/dto/CountryDto.java
@@ -0,0 +1,16 @@
+package com.riskgame.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CountryDto {
+
+	private int id;
+	private String countryName;
+	private String continentName;
+
+}
