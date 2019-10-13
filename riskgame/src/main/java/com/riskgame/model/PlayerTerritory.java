d5a8afe5c8cc8dbf2f611fad525347b71d54667c
Jaswanthjc <jaswanthbanawathu@gmail.com>
2019-10-12 23:50:14 -0400

    documenation added in playerTerritory

--- riskgame/src/main/java/com/riskgame/model/PlayerTerritory.java
+++ riskgame/src/main/java/com/riskgame/model/PlayerTerritory.java
@@ -8,15 +8,28 @@ import lombok.Setter;
 import lombok.ToString;
 
 /**
+ * This class stores the information regarding player's territories during gameplay.
  * 
- * @author <Koshaben Patel</a>
+ * @author <a href="mailto:ko_pate@encs.concordia.ca">Koshaben Patel</a>
+ * @author <a href="mailto:j_banava@encs.concordia.ca">Jaswanth Banavathu</a> documenation added
  */
 @Getter
 @Setter
 @ToString
 public class PlayerTerritory {
+	/**
+	 * name of the continent of that particular player's territory
+	 */
 	private String continentName;
+	
+	/**
+	 * Name of the territory owned by player
+	 */
 	private String territoryName;
+	
+	/**
+	 * Total number of army on that particular territory
+	 */
 	private int armyOnterritory;
 
 }
