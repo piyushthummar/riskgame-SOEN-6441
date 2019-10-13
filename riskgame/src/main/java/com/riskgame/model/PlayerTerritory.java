a994664b08313729b672cc6dbed9234c83bfcb7e
Jaswanthjc <jaswanthbanawathu@gmail.com>
2019-10-13 00:01:27 -0400

    lombok dependency comment

--- riskgame/src/main/java/com/riskgame/model/PlayerTerritory.java
+++ riskgame/src/main/java/com/riskgame/model/PlayerTerritory.java
@@ -13,7 +13,15 @@ import lombok.ToString;
  * @author <a href="mailto:ko_pate@encs.concordia.ca">Koshaben Patel</a>
  * @author <a href="mailto:j_banava@encs.concordia.ca">Jaswanth Banavathu</a> documenation added
  */
+
+/**
+ * lombok dependency to generate automatic getter method
+ */
 @Getter
+
+/**
+ * lombok dependency to generate automatic setter method
+ */
 @Setter
 @ToString
 public class PlayerTerritory {
