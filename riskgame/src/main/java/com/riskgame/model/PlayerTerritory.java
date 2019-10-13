8f32c4bb79299245eb72c771249fc2860810366e
Jaswanthjc <jaswanthbanawathu@gmail.com>
2019-10-13 00:28:44 -0400

    documentation

--- riskgame/src/main/java/com/riskgame/model/PlayerTerritory.java
+++ riskgame/src/main/java/com/riskgame/model/PlayerTerritory.java
@@ -10,6 +10,9 @@ import lombok.ToString;
 /**
  * This class stores the information regarding player's territories during gameplay.
  * 
+ * Three dependecies (Getter, Setter and ToString) you can see on the top of the class are lombok dependencies to 
+ * automatically generate getter, setter and tostring method in the code.
+ * 
  * @author <a href="mailto:ko_pate@encs.concordia.ca">Koshaben Patel</a>
  * @author <a href="mailto:j_banava@encs.concordia.ca">Jaswanth Banavathu</a> documenation added
  */
