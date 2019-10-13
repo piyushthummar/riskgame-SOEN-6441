48d192873c27c55ca69f9d8056857beff9fd9279
Kosha Patel <koshapatel1045@gmail.com>
2019-10-13 01:11:03 -0400

    PlayerHandlerImpl created

---
+++ riskgame/src/main/java/com/riskgame/service/Impl/PlayerHandlerImpl.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.riskgame.service.Impl;
+
+import com.riskgame.service.PlayerHandlerInterface;
+
+/**
+ * 
+ * @author <a href="mailto:ko_pate@encs.concordia.ca">Koshaben Patel</a>
+ */
+public class PlayerHandlerImpl implements PlayerHandlerInterface{
+
+}
