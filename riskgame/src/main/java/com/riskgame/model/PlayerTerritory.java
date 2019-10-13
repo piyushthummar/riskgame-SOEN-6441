4a0a1d5dcb5a996675b666f1a2f9d8bda8bfd85e
Kosha Patel <koshapatel1045@gmail.com>
2019-10-12 23:29:44 -0400

    PlayerTerritory created

---
+++ riskgame/src/main/java/com/riskgame/model/PlayerTerritory.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.riskgame.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * 
+ * @author <Koshaben Patel</a>
+ */
+@Getter
+@Setter
+@ToString
+public class PlayerTerritory {
+	private String continentName;
+	private String territoryName;
+	private int armyOnterritory;
+
+}
