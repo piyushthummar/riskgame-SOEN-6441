32c902bafb8d1dce69178fbdcb90143cc8a6cd42
Kosha Patel <koshapatel1045@gmail.com>
2019-10-12 23:41:51 -0400

    documentation added in player

--- riskgame/src/main/java/com/riskgame/model/Player.java
+++ riskgame/src/main/java/com/riskgame/model/Player.java
@@ -6,15 +6,32 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 /**
+ * Player class manages information of players and their territories
  * 
  * @author <a href="mailto:z_tel@encs.concordia.ca">Zankhanaben Patel</a>
+ * @author <a href="mailto:ko_pate@encs.concordia.ca">Koshaben Patel</a> documenation added
  */
 @Getter
 @Setter
 @ToString
 public class Player {
+	/**
+	 * Stores the id of player
+	 */
 	private int playerId;
+	
+	/**
+	 * Stores the name of player
+	 */
 	private String playerName;
+	
+	/**
+	 * The number of army owns by a player
+	 */
 	private int armyOwns;
+	
+	/**
+	 * List of Territories owns by player
+	 */
 	private List<PlayerTerritory> playerterritories;
 }
