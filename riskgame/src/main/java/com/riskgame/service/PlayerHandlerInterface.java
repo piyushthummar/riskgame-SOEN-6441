9fc0bbde55e6ea0e14662ff3afbb13ce15758b94
Kosha Patel <koshapatel1045@gmail.com>
2019-10-13 18:04:18 -0400

    getTerritories() added

--- riskgame/src/main/java/com/riskgame/service/PlayerHandlerInterface.java
+++ riskgame/src/main/java/com/riskgame/service/PlayerHandlerInterface.java
@@ -3,7 +3,10 @@
  */
 package com.riskgame.service;
 
+import java.util.List;
+
 import com.riskgame.model.GamePlayPhase;
+import com.riskgame.model.PlayerTerritory;
 import com.riskgame.model.RiskMap;
 
 /**
@@ -15,5 +18,7 @@ import com.riskgame.model.RiskMap;
 public interface PlayerHandlerInterface {
 	GamePlayPhase createPlayer(int noOfPlayer);
 	int findTotalArmy(int noOfPlayer);
+	void populateTerritoriesByRoundRobbin(RiskMap riskMap);
+	List<PlayerTerritory> getTerritories(RiskMap riskMap);
 	
 }
