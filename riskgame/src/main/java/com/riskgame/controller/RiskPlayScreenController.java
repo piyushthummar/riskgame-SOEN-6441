0218b4fed9b3f05536e7b819b783770d55b335df
Piyush Thummar <piyushthummar305@gmail.com>
2019-10-16 00:36:19 -0400

    added methods for reinforcement

--- riskgame/src/main/java/com/riskgame/controller/RiskPlayScreenController.java
+++ riskgame/src/main/java/com/riskgame/controller/RiskPlayScreenController.java
@@ -4,6 +4,11 @@
 package com.riskgame.controller;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.ResourceBundle;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +16,13 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 
 import com.riskgame.config.StageManager;
+import com.riskgame.model.Continent;
+import com.riskgame.model.GamePlayPhase;
+import com.riskgame.model.Player;
+import com.riskgame.model.PlayerTerritory;
+import com.riskgame.model.RiskMap;
+import com.riskgame.model.Territory;
+import com.riskgame.service.Impl.MapManagementImpl;
 import com.riskgame.view.FxmlView;
 
 import javafx.event.ActionEvent;
@@ -27,7 +39,7 @@ import javafx.scene.control.TextField;
  * @author <a href="mailto:p_thumma@encs.concordia.ca">Piyush Thummar</a>
  */
 @Controller
-public class RiskPlayScreenController implements Initializable{
+public class RiskPlayScreenController implements Initializable {
 
 	@FXML
 	private ListView<?> lvTerritoryList;
@@ -80,6 +92,9 @@ public class RiskPlayScreenController implements Initializable{
 	@FXML
 	private Button btnExit;
 
+	@Autowired
+	public MapManagementImpl mapManagementImpl;
+
 	@FXML
 	void placeReinforcement(ActionEvent event) {
 
@@ -102,22 +117,132 @@ public class RiskPlayScreenController implements Initializable{
 
 	@FXML
 	void fireCommand(ActionEvent event) {
+		txtConsoleLog.clear();
+		try {
+			String command = txtCommandLine.getText();
+			if (command != null && !command.isEmpty()) {
+				if (command.startsWith("reinforce")) {
+					txtConsoleLog.setText(placeReinforcement(command));
+				} else if (command.startsWith("fortify")) {
+					txtConsoleLog.setText(fortification(command));
+				} else
+					txtConsoleLog.setText("Please Enter Valid Command");
+			} else {
+				txtConsoleLog.setText("Please Enter valid Command");
+			}
 
+		} catch (Exception e) {
+			e.printStackTrace();
+			txtConsoleLog.setText("Please Enter Valid Command");
+		}
 	}
+
+	/**
+	 * @param command
+	 * @return
+	 */
+	private String fortification(String command) {
+
+		return null;
+	}
+
+	/**
+	 * @param command
+	 * @return
+	 */
+	private String placeReinforcement(String command) {
+		StringBuilder result = new StringBuilder();
+		Player player = new Player();
+		String countryName = "";
+		int armyToPlace;
+		String[] dataArray = command.split(" ");
+		List<String> commandData = Arrays.asList(dataArray);
+		if (commandData.size() != 3) {
+			result.append("Please Enter Valid Command");
+		} else {
+			countryName = commandData.get(1);
+			armyToPlace = Integer.parseInt(commandData.get(2));
+			reinforce(armyToPlace, countryName, player);
+		}
+		return result.toString();
+	}
+
+	/**
+	 * @param armyToMove
+	 * @param countryName
+	 * @param playerName
+	 */
+
+	private void reinforce(int armyToPlace, String countryName, Player player) {
+		GamePlayPhase gPhase = new GamePlayPhase();
+		int addExtraArmy = 0;
+		RiskMap riskmap = mapManagementImpl.readMap(gPhase.getFileName());
+		Map<Integer, Continent> continentMap = riskmap.getContinents();
+		List<Continent> continentList = new ArrayList<Continent>();
+		for (Entry<Integer, Continent> e : continentMap.entrySet()) {
+			continentList.add(e.getValue());
+		}
+		List<Player> playerList = gPhase.getGameState();
+		for (Player ply : playerList) {
+			List<PlayerTerritory> playerTerritories = ply.getPlayerterritories();
+			List<String> playerTerritoriesStringList = new ArrayList<>();
+			for (PlayerTerritory p : playerTerritories) {
+				playerTerritoriesStringList.add(p.getTerritoryName());
+			}
+			int totalOwnedCountries = playerTerritories.size();
+			for (PlayerTerritory playerTerritory : playerTerritories) {
+				if (countryName.equals(playerTerritory.getTerritoryName())) {
+					for (Continent ct : continentList) {
+						List<Territory> territoryList = ct.getTerritoryList();
+						int count = 0;
+						for (Territory territory : territoryList) {
+							if (playerTerritoriesStringList.contains(territory.getTerritoryName())) {
+								count++;
+							}
+						}
+						if (count == territoryList.size()) {
+							addExtraArmy = ct.getContinentValue();
+							break;
+						}
+					}
+					int totalArmyforReinforce = checkForReinforcement(totalOwnedCountries);
+					
+				}
+				break;
+			}
+			break;
+		}
+	}
+
+	/**
+	 * @param armyToPlace
+	 * @param totalOwnedCountries
+	 * @param addExtraArmy
+	 * @return 
+	 */
+	private int checkForReinforcement(int totalOwnedCountries) {
+		int total = Math.floorDiv(totalOwnedCountries, 3);
+		int totalArmyforReinforce = Math.max(total, 3);
+		return totalArmyforReinforce;
+		
+	}
+
 	@Lazy
 	@Autowired
 	private StageManager stageManager;
+
 	@FXML
 	void exitGame(ActionEvent event) {
 		stageManager.switchScene(FxmlView.WELCOME);
 	}
 
 	/**
-	 * @see javafx.fxml.Initializable#initialize(java.net.URL, java.util.ResourceBundle)
+	 * @see javafx.fxml.Initializable#initialize(java.net.URL,
+	 *      java.util.ResourceBundle)
 	 */
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+
 	}
 
 }
