f674ac24581cd6b56b9340b43e6bb2a37adee8e0
Raj Mistry <rajmistry2298@gmail.com>
2019-11-03 16:35:33 -0500

    RiskCard model created

---
+++ riskgame/src/main/java/com/riskgame/model/RiskCard.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.riskgame.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Model for Risk Cards
+ * 
+ * Two annotations (Getter, Setter) you can see on the top of the class are
+ * lombok dependencies to automatically generate getter, setter in the code.
+ * 
+ * @author <a href="mailto:r_istry@encs.concordia.ca">Raj Mistry</a>
+ */
+@Getter
+@Setter
+public class RiskCard {
+	
+	/**
+	 * This data member indicate card number
+	 */
+	private int cardNumber;
+	
+	/**
+	 * It indicate types of army i.e. Infantry, cavilary etc.
+	 */
+	private String armyType;
+	
+	@Override
+	public String toString() {
+		return "RiskCard [cardNumber=" + cardNumber + ", armyType=" + armyType + "]";
+	}	
+}
