4bfbf72aeb599964b0041dca6a09ef771c3aef94
Piyush Thummar <piyushthummar305@gmail.com>
2019-11-04 18:44:56 -0500

    methods created for card exhange in service

--- riskgame/src/main/java/com/riskgame/service/RiskPlayInterface.java
+++ riskgame/src/main/java/com/riskgame/service/RiskPlayInterface.java
@@ -1,6 +1,12 @@
 package com.riskgame.service;
 
+import java.util.List;
+import java.util.Map;
+
 import com.riskgame.model.GamePlayPhase;
+import com.riskgame.model.Player;
+import com.riskgame.model.RiskCard;
+import com.riskgame.model.RiskCardExchange;
 
 /**
  * This is a interface of RiskPlayGame buiness logic. Where reinforcement,attack
@@ -9,6 +15,8 @@ import com.riskgame.model.GamePlayPhase;
  * @author <a href="mailto:z_tel@encs.concordia.ca">Zankhanaben Patel</a>
  * @see com.riskgame.model.RiskMap
  * @see com.riskgame.model.Player
+ * @see com.riskgame.model.RiskCardExchange
+ * @see com.riskgame.model.RiskCard
  * @see com.riskgame.model.Territory com.riskgame.model.PlayerTerritory
  */
 public interface RiskPlayInterface {
@@ -23,8 +31,30 @@ public interface RiskPlayInterface {
 	/**
 	 * This method will make number of cards same as number of countries to use it at the phase of reinforcement
 	 * @param noOfCountries
+	 * @return 
 	 */
-	void makeCards(int noOfCountries);
+	Map<Integer, String> makeCards(int noOfCountries);
 	
+	/**
+	 * This method will be called to check if player control all the territories of particular continent
+	 * 
+	 * @param gamePlayPhase
+	 * @return value of continent control to update army
+	 */
 	int checkForContinentControlValue(GamePlayPhase gamePlayPhase);
+	
+	/**
+	 * This method will update armyCount after card exchange
+	 * 
+	 * @param player
+	 */
+	int updateArmyAfterCardExchange(Player player);
+	
+	/**
+	 * This method will be called after card exchange has been done and card need to add again to total card list
+	 * @param player
+	 * @param cards total cards in game
+	 * @param exchangedCards
+	 */
+	void updateCardListAfterExchange(Player player,List<RiskCard> cards,RiskCardExchange exchangedCards);
 }
