9d727763d30233d113f0be13e4b3595ebe7e1d55
Kosha Patel <koshapatel1045@gmail.com>
2019-11-20 23:25:10 -0500

    Documententation added for strategy classes

--- riskgame/src/main/java/com/riskgame/strategy/impl/RandomPlayer.java
+++ riskgame/src/main/java/com/riskgame/strategy/impl/RandomPlayer.java
@@ -3,10 +3,50 @@
  */
 package com.riskgame.strategy.impl;
 
+import com.riskgame.model.GamePlayPhase;
+import com.riskgame.strategy.StrategyInterface;
+
 /**
+ * Concrete implementation of Strategy interface of design pattern for Random
+ * strategy of player
  * 
  * @author <a href="mailto:z_tel@encs.concordia.ca">Zankhanaben Patel</a>
+ * @see com.riskgame.strategy
+ * @version 1.0.0
  */
-public class RandomPlayer {
+public class RandomPlayer implements StrategyInterface {
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see com.riskgame.strategy.StrategyInterface#reinforce(com.riskgame.model.GamePlayPhase)
+	 */
+	@Override
+	public GamePlayPhase reinforce(GamePlayPhase gps) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see com.riskgame.strategy.StrategyInterface#attack(com.riskgame.model.GamePlayPhase)
+	 */
+	@Override
+	public GamePlayPhase attack(GamePlayPhase gps) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see com.riskgame.strategy.StrategyInterface#fortify(com.riskgame.model.GamePlayPhase)
+	 */
+	@Override
+	public GamePlayPhase fortify(GamePlayPhase gps) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
