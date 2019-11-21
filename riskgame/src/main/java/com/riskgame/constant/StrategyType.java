a92c9f0a60a9134a9bd059816a7d045c16cd5543
Raj Mistry <rajmistry2298@gmail.com>
2019-11-21 04:01:52 -0500

    enum constants added for strategy

---
+++ riskgame/src/main/java/com/riskgame/constant/StrategyType.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.riskgame.constant;
+
+/**
+ * This is enum of different strategy
+ * 
+ * @author <a href="mailto:z_tel@encs.concordia.ca">Zankhanaben Patel</a>
+ * @version 1.0.0
+ */
+public enum StrategyType {
+	HUMAN, AGGRESIVE, BENEVOLENT, RANDOM, CHEATER
+}
