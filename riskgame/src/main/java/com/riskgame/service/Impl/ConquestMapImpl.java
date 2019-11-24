d4694d3bda8030bef9bc900b04d3c17aaaf848d4
Raj Mistry <rajmistry2298@gmail.com>
2019-11-24 02:01:54 -0500

    ReadMap method for conquest mapfile and related methods added

--- riskgame/src/main/java/com/riskgame/service/Impl/ConquestMapImpl.java
+++ riskgame/src/main/java/com/riskgame/service/Impl/ConquestMapImpl.java
@@ -1,14 +1,18 @@
 package com.riskgame.service.Impl;
 
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.springframework.util.StringUtils;
 
@@ -17,14 +21,26 @@ import com.riskgame.model.RiskMap;
 import com.riskgame.model.Territory;
 import com.riskgame.service.ConquestMapInterface;
 
+
 public class ConquestMapImpl implements ConquestMapInterface {
 
 	public static final String MAP_DIR_PATH = "src/main/resources/maps/";
 
+	enum FileDivider {
+		MAP, CONTINENT, TERRITORY;
+	}
+//	public static void main(String[] args) {
+//		ConquestMapImpl impl = new ConquestMapImpl();
+//		impl.readMap("India.map");
+//	}
+
+	private FileDivider currentSection;
+	private Map<Integer, Continent> continentMap;
+	private List<Territory> territoryList;
+
 	@Override
 	public boolean saveMapToFile(RiskMap riskMap)
 			throws UnsupportedEncodingException, FileNotFoundException, IOException {
-		// TODO Auto-generated method stub
 		boolean result = false;
 
 		try (PrintStream fileWriter = new PrintStream(
@@ -80,7 +96,6 @@ public class ConquestMapImpl implements ConquestMapInterface {
 						}
 
 					}
-
 				}
 				fileWriter.println();
 			}
@@ -100,8 +115,94 @@ public class ConquestMapImpl implements ConquestMapInterface {
 
 	@Override
 	public RiskMap readMap(String fileName) {
-		// TODO Auto-generated method stub
-		return null;
+
+		String mapline = "";
+		RiskMap riskMap = new RiskMap();
+		continentMap = new HashMap<Integer, Continent>();
+		territoryList = new ArrayList<Territory>();
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(MAP_DIR_PATH + fileName))) {
+			int continentCount = 1;
+			int territoryCount = 1;
+
+			while ((mapline = bufferedReader.readLine()) != null) {
+				if (mapline != null && !mapline.isEmpty()) {
+					riskMap.setMapName(fileName);
+					if (mapline.equalsIgnoreCase("[Map]")) {
+						currentSection = FileDivider.MAP;
+						mapline = bufferedReader.readLine();
+					} else if (mapline.equalsIgnoreCase("[Continents]")) {
+						currentSection = FileDivider.CONTINENT;
+						mapline = bufferedReader.readLine();
+					} else if (mapline.equalsIgnoreCase("[TERRITORIES]")) {
+						currentSection = FileDivider.TERRITORY;
+						mapline = bufferedReader.readLine();
+					}
+					if (currentSection == FileDivider.MAP)
+						continue;
+					if (currentSection == FileDivider.CONTINENT) {
+						setContinentsToMapObject(mapline, continentCount);
+						continentCount++;
+					}
+					if (currentSection == FileDivider.TERRITORY) {
+						setTerritoriesToMapObject(mapline, territoryCount);
+						territoryCount++;
+					}
+				}
+			}
+			setTerritoryListinContinent();
+			riskMap.setContinents(continentMap);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(riskMap);
+		return riskMap;
 	}
 
+	private void setTerritoryListinContinent() {
+		for (Entry<Integer, Continent> entry : continentMap.entrySet()) {
+			Continent c = entry.getValue();
+			List<Territory> continentsTerritoryList = new ArrayList<Territory>();
+			for (Territory territory : territoryList) {
+				if (territory.getContinentIndex() == entry.getKey()) {
+					continentsTerritoryList.add(territory);
+				}
+			}
+			c.setTerritoryList(continentsTerritoryList);
+		}
+	}
+
+	private void setTerritoriesToMapObject(String mapline, int territoryCount) {
+		if (mapline.length() > 0) {
+			List<String> neighbourTerritories = new ArrayList<String>();
+			Territory territory = new Territory();
+			territory.setTerritoryIndex(territoryCount);
+			String[] strArray = mapline.split(",");
+			int length = strArray.length;
+			territory.setTerritoryName(strArray[0]);
+			territory.setXAxis(Integer.parseInt(strArray[1]));
+			territory.setYAxis(Integer.parseInt(strArray[2]));
+			for (Entry<Integer, Continent> entry : continentMap.entrySet()) {
+				if (entry.getValue().getContinentName().equals(strArray[3])) {
+					territory.setContinentIndex(entry.getKey());
+				}
+			}
+			for (int i = 4; i < length; i++) {
+				neighbourTerritories.add(strArray[i].trim());
+			}
+			territory.setNeighbourTerritories(neighbourTerritories);
+			territoryList.add(territory);
+		}
+	}
+
+	private void setContinentsToMapObject(String mapline, int continentCount) {
+		if (mapline.length() > 0) {
+			Continent continent = new Continent();
+			String[] strArray = mapline.split("=");
+			continent.setContinentName(strArray[0].trim());
+			continent.setContinentValue(Integer.parseInt(strArray[1].trim()));
+			continent.setContinentIndex(continentCount);
+			continentMap.put(continentCount, continent);
+		}
+	}
 }
