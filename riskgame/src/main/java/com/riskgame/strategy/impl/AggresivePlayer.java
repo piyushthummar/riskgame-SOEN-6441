6ef83aeb7c16358588b15745ebd8c451bce235f3
Zankhana Patel <zankhanapatel1995.zp@gmail.com>
2019-11-24 19:17:02 -0500

    Completed AggresivePlayer all method and partial completed benevolent

--- riskgame/src/main/java/com/riskgame/strategy/impl/AggresivePlayer.java
+++ riskgame/src/main/java/com/riskgame/strategy/impl/AggresivePlayer.java
@@ -3,9 +3,13 @@
  */
 package com.riskgame.strategy.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 import com.riskgame.model.GamePlayPhase;
 import com.riskgame.model.Player;
 import com.riskgame.model.PlayerTerritory;
+import com.riskgame.model.RiskCard;
 import com.riskgame.service.MapManagementInterface;
 import com.riskgame.service.RiskPlayInterface;
 import com.riskgame.service.Impl.MapManagementImpl;
@@ -77,10 +81,28 @@ public class AggresivePlayer implements StrategyInterface {
 					}
 				}
 
-				//playerStrongestTerritory = riskPlayImpl.findStrongestTerritory(currentPlayer);
+				playerStrongestTerritory = riskPlayImpl.getStrongestTerritory(currentPlayer);
+				
+				if (playerStrongestTerritory != null) {
+					playerStrongestTerritory.setArmyOnterritory(
+							playerStrongestTerritory.getArmyOnterritory() + currentPlayer.getPlayerReinforceArmy());
+					currentPlayer.setPlayerReinforceArmy(0);
+					
+					
+					sb.append("Strongest Territory :").append(playerStrongestTerritory.getTerritoryName())
+					.append("Reinforced New Army count on strongest territory:")
+					.append(playerStrongestTerritory.getArmyOnterritory()).append(gamePlayPhase.getStatus())
+					.append(NEWLINE);
+				}
 			}
+
+			
 		}
+
 		
+
+		gamePlayPhase.setStatus(sb.toString());
+
 		return gamePlayPhase;
 	}
 
@@ -91,7 +113,120 @@ public class AggresivePlayer implements StrategyInterface {
 	 */
 	@Override
 	public GamePlayPhase attack(GamePlayPhase gamePlayPhase) {
-		// TODO Auto-generated method stub
+		Player currentPlayer = null;
+		sb = new StringBuilder();
+		PlayerTerritory playerStrongestTerritory = null;
+		List<String> playerCountry = new ArrayList<>();
+		String fromCountry = null;
+		String toCountry = null;
+		// For finding Strongest Territory
+		for (Player player : gamePlayPhase.getPlayerList()) {
+			if (player.getPlayerId() == gamePlayPhase.getCurrentPlayerId()) {
+				currentPlayer = player;
+				int max = 0;
+
+				for (PlayerTerritory territory : player.getPlayerterritories()) {
+					playerCountry.add(territory.getTerritoryName());
+					if (territory.getArmyOnterritory() > max) {
+						max = territory.getArmyOnterritory();
+						playerStrongestTerritory = territory;
+						fromCountry = playerStrongestTerritory.getTerritoryName();
+					}
+				}
+				break;
+			}
+		}
+
+		if (playerStrongestTerritory.getArmyOnterritory() > 1) {
+
+			List<String> neighbourCountriesList = mapManagementImpl
+					.getNeighbourCountriesListByCountryName(gamePlayPhase.getRiskMap(), fromCountry);
+			for (String neighbourCountry : neighbourCountriesList) {
+				if (!playerCountry.contains(neighbourCountry)) {
+					toCountry = neighbourCountry;
+					break;
+				}
+			}
+
+			if (toCountry != null) {
+
+				int armyOnFromC = playerStrongestTerritory.getArmyOnterritory();
+				int armyOnToC = riskPlayImpl.getCurrentAramyByCountryName(toCountry, gamePlayPhase.getPlayerList());
+
+				int attackerDice = riskPlayImpl.getAttackerDiesCount(armyOnFromC);
+				int defenderDice = riskPlayImpl.getDefenderDiceCount(armyOnToC);
+
+				if (attackerDice > 0 && defenderDice > 0) {
+
+					List<Integer> attackerList = riskPlayImpl.getCountFromDies(attackerDice);
+					List<Integer> defenderList = riskPlayImpl.getCountFromDies(defenderDice);
+					int min = Math.min(attackerList.size(), defenderList.size());
+
+					for (int i = 0; i < min; i++) {
+
+						if (defenderList.get(i) == attackerList.get(i)) {
+							updateArmyAfterBattle(fromCountry, "attacker", gamePlayPhase);
+						} else if (defenderList.get(i) > attackerList.get(i)) {
+							updateArmyAfterBattle(fromCountry, "attacker", gamePlayPhase);
+						} else if (defenderList.get(i) < attackerList.get(i)) {
+							updateArmyAfterBattle(fromCountry, "defender", gamePlayPhase);
+						}
+					}
+
+					Player defenderPlayer = riskPlayImpl.getPlayerByCountry(toCountry, gamePlayPhase.getPlayerList());
+					PlayerTerritory toTerritory = riskPlayImpl.getPlayerTerritoryByCountryName(toCountry,
+							defenderPlayer);
+
+					if (toTerritory.getArmyOnterritory() == 0) {
+
+						// allOutTerritoryConqured = true;
+						// attackMove = true;
+						// alloutFinish = true;
+
+						moveCountryToWinPlayer(fromCountry, toCountry, gamePlayPhase);
+
+						Player attackerPlayer = riskPlayImpl.getPlayerByCountry(fromCountry,
+								gamePlayPhase.getPlayerList());
+						PlayerTerritory fromTerritory = riskPlayImpl.getPlayerTerritoryByCountryName(fromCountry,
+								attackerPlayer);
+
+						if (fromTerritory.getArmyOnterritory() > 1) {
+							int armyToMove = fromTerritory.getArmyOnterritory() - 1;
+							moveArmy(fromCountry, toCountry, armyToMove, gamePlayPhase);
+						}
+						
+						if(gamePlayPhase.getRiskCardList().size()>0) {
+							
+							RiskCard riskCard = gamePlayPhase.getRiskCardList().get(0);
+							currentPlayer.getCardListOwnedByPlayer().add(riskCard);
+							sb.append(riskCard).append(" Assigned to ").append(currentPlayer.getPlayerName()).append(NEWLINE);
+							
+							gamePlayPhase.getRiskCardList().remove(0);
+							
+						}
+
+					}
+
+				} else {
+					// alloutFinish = true;
+					sb.append("Allout Attack not possible - not an valid army on country").append(NEWLINE);
+				}
+
+			} else {
+				sb.append(
+						"Attack is not possible because player's strongest countries all neighbour countries are part of player's country!");
+			}
+
+		} else {
+
+			sb.append("Can't attack because strongest selected territory : ")
+					.append(playerStrongestTerritory.getTerritoryName()).append(" has only ")
+					.append(playerStrongestTerritory.getArmyOnterritory()).append(" army ").append(NEWLINE);
+		}
+
+		gamePlayPhase.setGamePhase("ATTACK");
+		riskPlayImpl.checkForWinner(gamePlayPhase);
+		gamePlayPhase.setStatus(sb.toString() + gamePlayPhase.getStatus());
 		return gamePlayPhase;
 	}
 
@@ -102,7 +237,160 @@ public class AggresivePlayer implements StrategyInterface {
 	 */
 	@Override
 	public GamePlayPhase fortify(GamePlayPhase gamePlayPhase) {
-		// TODO Auto-generated method stub
+
+		Player currentPlayer = null;
+		Boolean isneighbour = false;
+
+		String aggressive_fortify_message = "";
+		String old_message = "";
+
+		for (Player player : gamePlayPhase.getPlayerList()) {
+			if (player.getPlayerId() == gamePlayPhase.getCurrentPlayerId()) {
+				currentPlayer = player;
+				break;
+			} else {
+				continue;
+			}
+		}
+
+		if (currentPlayer != null) {
+
+			for (PlayerTerritory territory_a : currentPlayer.getPlayerterritories()) {
+
+				if (territory_a.getArmyOnterritory() == 1) {
+					continue;
+				}
+				for (PlayerTerritory territory_b : currentPlayer.getPlayerterritories()) {
+
+					isneighbour = false;
+
+					if (territory_a.getTerritoryName().equalsIgnoreCase(territory_b.getTerritoryName())
+							|| territory_b.getArmyOnterritory() == 1) {
+						continue;
+					} else {
+
+						List<String> neighbourCountriesList = mapManagementImpl.getNeighbourCountriesListByCountryName(
+								gamePlayPhase.getRiskMap(), territory_a.getTerritoryName());
+						if (neighbourCountriesList.contains(territory_b.getTerritoryName())) {
+							isneighbour = true;
+						}
+					}
+
+					if (isneighbour == true) {
+
+						old_message = (territory_b.getArmyOnterritory() - 1) + " army moved from "
+								+ territory_b.getTerritoryName() + " to " + territory_a.getTerritoryName() + "\n";
+						aggressive_fortify_message = old_message + aggressive_fortify_message;
+						territory_a.setArmyOnterritory(
+								territory_a.getArmyOnterritory() + territory_b.getArmyOnterritory() - 1);
+						territory_b.setArmyOnterritory(1);
+					}
+				}
+			}
+		}
+
+		gamePlayPhase.setStatus(aggressive_fortify_message);
+		return gamePlayPhase;
+	}
+
+	/**
+	 * This method will update number of army from attcker or defender territory
+	 * after battle completed everytime
+	 * 
+	 * @param country
+	 * @param name    attcker or defender
+	 */
+	private GamePlayPhase updateArmyAfterBattle(String country, String name, GamePlayPhase gamePlayPhase) {
+		// sb = new StringBuilder();
+		for (Player player : gamePlayPhase.getPlayerList()) {
+
+			for (PlayerTerritory playerTerritory : player.getPlayerterritories()) {
+
+				if (country.equalsIgnoreCase(playerTerritory.getTerritoryName())) {
+
+					playerTerritory.setArmyOnterritory(playerTerritory.getArmyOnterritory() - 1);
+					player.setArmyOwns(player.getArmyOwns() - 1);
+
+					sb.append(name).append(" Country loses 1 army ").append(name).append(" has left with ")
+							.append(playerTerritory.getArmyOnterritory()).append(NEWLINE);
+
+					break;
+
+				}
+			}
+		}
+		gamePlayPhase.setStatus(gamePlayPhase.getStatus() + sb.toString());
+		return gamePlayPhase;
+	}
+
+	/**
+	 * This method will move conquered country to attacker
+	 * 
+	 * @param fromCountryAttack
+	 * @param toCountryAttack
+	 */
+	private GamePlayPhase moveCountryToWinPlayer(String fromCountryAttack, String toCountryAttack,
+			GamePlayPhase gamePlayPhase) {
+		// sb = new StringBuilder();
+		Player player = riskPlayImpl.getPlayerByCountry(toCountryAttack, gamePlayPhase.getPlayerList());
+		PlayerTerritory playerTerritory = riskPlayImpl.getPlayerTerritoryByCountryName(toCountryAttack, player);
+
+		// Fore removing country
+		for (Player playertoLst : gamePlayPhase.getPlayerList()) {
+
+			ListIterator<PlayerTerritory> playerTerritories = playertoLst.getPlayerterritories().listIterator();
+
+			while (playerTerritories.hasNext()) {
+				if (playerTerritories.next().getTerritoryName().equalsIgnoreCase(playerTerritory.getTerritoryName())) {
+					playerTerritories.remove();
+				}
+			}
+		}
+		// for Adding country
+		for (Player playerFromLst : gamePlayPhase.getPlayerList()) {
+
+			ListIterator<PlayerTerritory> playerTerritories = playerFromLst.getPlayerterritories().listIterator();
+
+			while (playerTerritories.hasNext()) {
+				if (playerTerritories.next().getTerritoryName().equalsIgnoreCase(fromCountryAttack)) {
+					playerTerritories.add(playerTerritory);
+				}
+			}
+		}
+		sb.append(toCountryAttack).append(" country has been conquered ").append(NEWLINE);
+		return gamePlayPhase;
+	}
+
+	/**
+	 * This method will move army from attcker country to conqured country
+	 * 
+	 * @param fromCountryAttack
+	 * @param toCountryAttack
+	 * @param armyToMove
+	 */
+	private GamePlayPhase moveArmy(String fromCountryAttack, String toCountryAttack, int armyToMove,
+			GamePlayPhase gamePlayPhase) {
+		// sb = new StringBuilder();
+		for (Player player : gamePlayPhase.getPlayerList()) {
+
+			for (PlayerTerritory playerTerritory : player.getPlayerterritories()) {
+
+				if (toCountryAttack.equalsIgnoreCase(playerTerritory.getTerritoryName())) {
+
+					playerTerritory.setArmyOnterritory(playerTerritory.getArmyOnterritory() + armyToMove);
+
+					sb.append(armyToMove).append(" Army Moved Successfully on ").append(toCountryAttack)
+							.append(" country").append(NEWLINE);
+
+				} else if (fromCountryAttack.equalsIgnoreCase(playerTerritory.getTerritoryName())) {
+
+					playerTerritory.setArmyOnterritory(playerTerritory.getArmyOnterritory() - armyToMove);
+
+				}
+			}
+		}
+
+		gamePlayPhase.setStatus(gamePlayPhase.getStatus() + sb.toString());
 		return gamePlayPhase;
 	}
 
--- riskgame/src/main/java/com/riskgame/strategy/impl/BenevolentPlayer.java
+++ riskgame/src/main/java/com/riskgame/strategy/impl/BenevolentPlayer.java
@@ -3,7 +3,12 @@
  */
 package com.riskgame.strategy.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.riskgame.model.GamePlayPhase;
+import com.riskgame.model.Player;
+import com.riskgame.model.PlayerTerritory;
 import com.riskgame.service.MapManagementInterface;
 import com.riskgame.service.RiskPlayInterface;
 import com.riskgame.service.Impl.MapManagementImpl;
@@ -20,6 +25,9 @@ import com.riskgame.strategy.StrategyInterface;
  */
 public class BenevolentPlayer implements StrategyInterface {
 
+	public static StringBuilder sb;
+	private static String NEWLINE = System.getProperty("line.separator");
+
 	private MapManagementInterface mapManagementImpl;
 	private RiskPlayInterface riskPlayImpl;
 
@@ -35,7 +43,40 @@ public class BenevolentPlayer implements StrategyInterface {
 	 */
 	@Override
 	public GamePlayPhase reinforce(GamePlayPhase gamePlayPhase) {
-		// TODO Auto-generated method stub
+		sb = new StringBuilder();
+		Player currentPlayer = null;
+		PlayerTerritory playerWeakestTerritory = null;
+		if (gamePlayPhase != null) {
+			for (Player player : gamePlayPhase.getPlayerList()) {
+				if (player.getPlayerId() == gamePlayPhase.getCurrentPlayerId()) {
+					currentPlayer = player;
+					break;
+				} else {
+					continue;
+				}
+			}
+			if (currentPlayer != null) {
+
+				playerWeakestTerritory = riskPlayImpl.getWeakestTerritory(currentPlayer);
+
+				if (playerWeakestTerritory != null) {
+
+					playerWeakestTerritory.setArmyOnterritory(
+							playerWeakestTerritory.getArmyOnterritory() + currentPlayer.getPlayerReinforceArmy());
+					currentPlayer.setPlayerReinforceArmy(0);
+
+					sb.append("weakest Territory :").append(playerWeakestTerritory.getTerritoryName())
+							.append("Reinforced New Army count on weakest territory:")
+							.append(playerWeakestTerritory.getArmyOnterritory()).append(gamePlayPhase.getStatus())
+							.append(NEWLINE);
+
+				}
+			}
+
+		}
+
+		gamePlayPhase.setStatus(sb.toString());
+
 		return gamePlayPhase;
 	}
 
@@ -46,7 +87,7 @@ public class BenevolentPlayer implements StrategyInterface {
 	 */
 	@Override
 	public GamePlayPhase attack(GamePlayPhase gamePlayPhase) {
-		// TODO Auto-generated method stub
+		gamePlayPhase.setStatus("Benevolent Player will never attack \n");
 		return gamePlayPhase;
 	}
 
@@ -57,8 +98,66 @@ public class BenevolentPlayer implements StrategyInterface {
 	 */
 	@Override
 	public GamePlayPhase fortify(GamePlayPhase gamePlayPhase) {
-		// TODO Auto-generated method stub
+
+		sb = new StringBuilder();
+		Player currentPlayer = null;
+		PlayerTerritory playerWeakestTerritory = null;
+		if (gamePlayPhase != null) {
+
+			for (Player player : gamePlayPhase.getPlayerList()) {
+				if (player.getPlayerId() == gamePlayPhase.getCurrentPlayerId()) {
+					currentPlayer = player;
+					break;
+				} else {
+					continue;
+				}
+			}
+
+			if (currentPlayer != null) {
+
+				playerWeakestTerritory = riskPlayImpl.getWeakestTerritory(currentPlayer);
+				if (playerWeakestTerritory != null) {
+					List<Integer> armies_no_list = getListofArmies(currentPlayer.getPlayerterritories());
+					
+					if(armies_no_list.size()>0) {
+						
+						
+						List<PlayerTerritory> weak_territories_list = getWeakTerritories(armies_no_list,currentPlayer.getPlayerterritories());
+						
+						
+						
+					}
+					
+					
+					
+				}
+
+			}
+
+		}
+
 		return gamePlayPhase;
+	}
+
+	private List<PlayerTerritory> getWeakTerritories(List<Integer> armies_no_list,
+			List<PlayerTerritory> playerterritories) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * This method will get list of armies values on each territory of current
+	 * player
+	 * 
+	 * @param playerTerritoriesList List of Current Player Territories
+	 * @return List of army values on each territory
+	 */
+	public List<Integer> getListofArmies(List<PlayerTerritory> playerTerritoriesList) {
+		List<Integer> armies = new ArrayList<>();
+		for (PlayerTerritory territory : playerTerritoriesList) {
+			armies.add(territory.getArmyOnterritory());
+		}
+		return armies;
 	}
 
 }
